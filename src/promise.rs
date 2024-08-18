95b49184c11c724fa4060039870f593bf9b8d0de
Takeru Ohta <phjgt308@gmail.com>
2024-08-18 16:17:14 +0900

    Add promise docs

--- src/promise.rs
+++ src/promise.rs
@@ -1,9 +1,17 @@
 use crate::{LogPosition, MessageSeqNum, Node, Term};
 
+/// Promise of a commit that results in either rejection or acceptance.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum CommitPromise {
+    /// The promise is pending.
+    ///
+    /// This state can be updated to either [`CommitPromise::Accepted`] or [`CommitPromise::Rejected`] by invoking [`CommitPromise::poll()`].
     Pending(LogPosition),
+
+    /// The promise is rejected.
     Rejected,
+
+    /// The promise is accepted.
     Accepted,
 }
 
@@ -12,6 +20,9 @@ impl CommitPromise {
         Self::Pending(position)
     }
 
+    /// Polls the promise to update its state.
+    ///
+    /// For convinience, the updated promise is returned.
     pub fn poll(&mut self, node: &Node) -> Self {
         let Self::Pending(position) = *self else {
             return *self;
@@ -24,51 +35,68 @@ impl CommitPromise {
         *self
     }
 
+    /// Returns [`true`] if the promise is pending.
     pub fn is_pending(&self) -> bool {
         matches!(self, Self::Pending(..))
     }
 
+    /// Returns [`true`] if the promise is rejected.
     pub fn is_rejected(&self) -> bool {
         matches!(self, Self::Rejected)
     }
 
+    /// Returns [`true`] if the promise is accepted.
     pub fn is_accepted(&self) -> bool {
         matches!(self, Self::Accepted)
     }
 }
 
+/// Promise of a heartbeat that results in either rejection or acceptance.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum HeartbeatPromise {
-    Pending { term: Term, sn: MessageSeqNum },
+    /// The promise is pending.
+    ///
+    /// This state can be updated to either [`HeartbeatPromise::Accepted`] or [`HeartbeatPromise::Rejected`] by invoking [`HeartbeatPromise::poll()`].
+    Pending(Term, MessageSeqNum),
+
+    /// The promise is rejected.
     Rejected,
+
+    /// The promise is accepted.
     Accepted,
 }
 
 impl HeartbeatPromise {
-    pub(crate) fn new(term: Term, sn: MessageSeqNum) -> Self {
-        Self::Pending { term, sn }
+    pub(crate) fn new(term: Term, seqno: MessageSeqNum) -> Self {
+        Self::Pending(term, seqno)
     }
 
+    /// Polls the promise to update its state.
+    ///
+    /// For convinience, the updated promise is returned.
     pub fn poll(&mut self, node: &Node) -> Self {
-        let Self::Pending { term, sn } = *self else {
+        let Self::Pending(term, seqno) = *self else {
             return *self;
         };
         if node.current_term() != term {
             *self = Self::Rejected;
-        } else if sn <= node.leader_sn {
+        } else if seqno <= node.leader_sn {
             *self = Self::Accepted;
         }
         *self
     }
 
+    /// Returns [`true`] if the promise is pending.
     pub fn is_pending(&self) -> bool {
         matches!(self, Self::Pending { .. })
     }
 
+    /// Returns [`true`] if the promise is rejected.
     pub fn is_rejected(&self) -> bool {
         matches!(self, Self::Rejected)
     }
 
+    /// Returns [`true`] if the promise is accepted.
     pub fn is_accepted(&self) -> bool {
         matches!(self, Self::Accepted)
     }
