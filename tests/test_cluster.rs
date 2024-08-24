596ec8c754bb6957870b8778ff8179b3eb0e5e4f
Takeru Ohta <phjgt308@gmail.com>
2024-08-25 08:27:30 +0900

    Update test_cluster.rs

--- tests/test_cluster.rs
+++ tests/test_cluster.rs
@@ -3,21 +3,83 @@ use raftbare::{Node, NodeId};
 #[derive(Debug)]
 pub struct TestCluster {
     pub nodes: Vec<TestNode>,
+    pub now: TestClock,
+}
+
+#[derive(Debug, Clone)]
+pub struct TestLinkOptions {
+    pub latency_ticks: MinMax,
+    pub drop_rate: f32,
+}
+
+impl Default for TestLinkOptions {
+    fn default() -> Self {
+        Self {
+            latency_ticks: MinMax::new(5, 20),
+            drop_rate: 0.0,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TestNodeOptions {
+    pub election_timeout_ticks: MinMax,
+    pub storage_latency_ticks: MinMax,
+    pub install_snapshot_ticks: MinMax,
+    pub running_ticks: MinMax,
+    pub stopping_ticks: MinMax,
+    pub log_entries_lost: MinMax,
+    pub max_entries_per_rpc: usize,
+    pub voter: bool,
+}
+
+impl Default for TestNodeOptions {
+    fn default() -> Self {
+        Self {
+            election_timeout_ticks: MinMax::new(100, 1000),
+            storage_latency_ticks: MinMax::new(1, 10),
+            install_snapshot_ticks: MinMax::new(1000, 10_000),
+            running_ticks: MinMax::constant(usize::MAX),
+            stopping_ticks: MinMax::constant(usize::MAX),
+            log_entries_lost: MinMax::constant(0),
+            max_entries_per_rpc: 100,
+            voter: true,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct MinMax {
+    pub min: usize,
+    pub max: usize,
+}
+
+impl MinMax {
+    pub fn new(min: usize, max: usize) -> MinMax {
+        assert!(min <= max);
+        MinMax { min, max }
+    }
+
+    pub fn constant(value: usize) -> MinMax {
+        MinMax::new(value, value)
+    }
 }
 
 #[derive(Debug)]
 pub struct TestNode {
     pub inner: Node,
+    pub options: TestNodeOptions,
     pub running: bool,
     pub timeout_expire_time: Option<TestClock>,
     pub storage_finish_time: Option<TestClock>,
 }
 
 impl TestNode {
-    pub fn start(id: NodeId) -> TestNode {
+    pub fn new(id: NodeId, running: bool, options: TestNodeOptions) -> TestNode {
         TestNode {
             inner: Node::start(id),
-            running: true,
+            options,
+            running,
             timeout_expire_time: None,
             storage_finish_time: None,
         }
@@ -25,7 +87,7 @@ impl TestNode {
 }
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub struct TestClock(u64);
+pub struct TestClock(usize);
 
 impl TestClock {
     pub fn new() -> TestClock {
@@ -35,21 +97,4 @@ impl TestClock {
     pub fn tick(&mut self) {
         self.0 += 1;
     }
-}
-
-#[derive(Debug)]
-pub enum Step {
-    AddVoterNode,
-    RemoveVoterNode,
-    AddNonVoterNode,
-    RemoveNonVoterNode,
-    ChangeConfig,
-    Heartbeat,
-    ProposeCommand,
-    InstallSnapshot,
-    SetLinkDelay,
-    SetLinkPacketLoss,
-    SetStorageLatency,
-    RestartNode,
-    RestartNodeWithLogTruncate,
 }
