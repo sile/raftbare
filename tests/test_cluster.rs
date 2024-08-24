755109060cf6c85c00b11ee59664b11370a79ed2
Takeru Ohta <phjgt308@gmail.com>
2024-08-25 07:41:58 +0900

    Add rand for dev

--- tests/test_cluster.rs
+++ tests/test_cluster.rs
@@ -1,8 +1,40 @@
-use raftbare::Node;
+use raftbare::{Node, NodeId};
 
 #[derive(Debug)]
 pub struct TestCluster {
-    pub nodes: Vec<Node>,
+    pub nodes: Vec<TestNode>,
+}
+
+#[derive(Debug)]
+pub struct TestNode {
+    pub inner: Node,
+    pub running: bool,
+    pub timeout_expire_time: Option<TestClock>,
+    pub storage_finish_time: Option<TestClock>,
+}
+
+impl TestNode {
+    pub fn start(id: NodeId) -> TestNode {
+        TestNode {
+            inner: Node::start(id),
+            running: true,
+            timeout_expire_time: None,
+            storage_finish_time: None,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct TestClock(u64);
+
+impl TestClock {
+    pub fn new() -> TestClock {
+        TestClock(0)
+    }
+
+    pub fn tick(&mut self) {
+        self.0 += 1;
+    }
 }
 
 #[derive(Debug)]
